6efca8212eecaf3a23bb65470a2fb62807f4fb8a
nurulla-hasan <nurullahasan.dev@gmail.com>
2025-05-20 19:49:58 +0600

    login and logout api created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import { User } from "../models/userModel.js";
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const register = async (req, res) => {
     try {
@@ -47,3 +48,53 @@ export const register = async (req, res) => {
         return res.status(500).json({ message: "Server error" });
     }
 };
+
+
+export const login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        if (!username || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+        const user = await User.findOne({ username })
+        if (!user) {
+            return res.status(400).json({
+                message: 'Incorrect Username or password',
+                success: false
+            })
+        };
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            return res.status(400).json({
+                message: 'Incorrect Username or password',
+                success: false
+            })
+        };
+
+        const tokenData = {
+            userId: user._id
+        };
+        const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, { expiresIn: '1d' })
+
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'strict' }).json({
+            _id: user._id,
+            username: user.username,
+            fullName: user.fullName,
+            profilePhoto: user.profilePhoto
+
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const logout = (req, res) => {
+    try {
+        return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+            message:"logged out successfully"
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
